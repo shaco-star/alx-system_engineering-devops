773450292e9c0145a87cd90464bb2b5750a0d801
shaco-star <sherifm846@gmail.com>
2023-08-25 04:39:10 -0400

    commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#create file in /temp directory
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
