a94c08138e5069762ddaab7ee5002454de4f0688
shaco-star <sherifm846@gmail.com>
2023-10-19 13:07:29 -0700

    commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,14 @@
 # This Puppet manifest increases the amount of traffic an Nginx server can handle.
 
 
-# Increase the ULIMIT of the default file
+# Increase the ULIMIT of the default file in nginx
 exec { 'fix--for-nginx':
   command => 'test -f /etc/default/nginx && sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
-  require => Package['nginx'],
 } ->
 
-# Restart Nginx
+# Restart Nginx server
 exec { 'nginx-restart':
   command     => 'nginx restart',
   path        => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/etc/init.d/',
-  refreshonly => true,
-  subscribe   => Exec['fix--for-nginx'],
 }
