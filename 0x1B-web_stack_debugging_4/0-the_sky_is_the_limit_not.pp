3c5298afe4768b842053867210f4a4889a92b0b7
shaco-star <sherifm846@gmail.com>
2023-10-19 13:11:55 -0700

    commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -9,6 +9,6 @@ exec { 'fix--for-nginx':
 
 # Restart Nginx server
 exec { 'nginx-restart':
-  command     => 'nginx restart',
-  path        => '/etc/init.d/'
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
